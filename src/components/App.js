802be0f78e026819fa082aed1e292459730e28d4
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-16 11:45:50 +0300

    add streams and header components

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import {BrowserRouter,Route,Link} from 'react-router-dom';
+import {BrowserRouter,Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
-const PageOne = ()=>{
-    return <div>
-        Page One
-        <Link to ="/pagetwo">Navigate to page two</Link>
-        </div>
-}
-const PageTwo = ()=>{
-    return <div>
-        Page Two
-        <button>Click Me!</button>
-        <Link to ="/">Navigate to page one</Link>
-        </div>
-}
 const App = ()=>{
     return (
+     <div>
         <BrowserRouter>
         <div>
-          <Route path = "/" exact component ={PageOne}/>
-          <Route path = "/pagetwo" component ={PageTwo}/>
+          <Header />
+          <Route path = "/" exact component = {StreamList}/>
+          <Route path = "/streams/new" exact component = {StreamCreate}/>
+          <Route path = "/streams/edit" exact component = {StreamEdit}/>
+          <Route path = "/streams/delete" exact component = {StreamDelete}/>
+          <Route path = "/streams/show" exact component = {StreamShow}/>
         </div>
         </BrowserRouter>
+     </div>
     )
 };
 export default App;
