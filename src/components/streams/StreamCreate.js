c96db2215886fb341b2f939d8dbaa91d47c4b4c9
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-19 15:35:31 +0300

    add changes to StreamCreate component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,12 +2,21 @@ import React from 'react';
 import {Field,reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component{
-    renderInput({input,label,meta}){
+    renderError({error,touched}){
+        if(error && touched){
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+    renderInput=({input,label,meta})=>{
         return (
          <div className="field">
           <label>{label}</label>
-           <input {...input} />
-           <div>{meta.error}</div>
+           <input {...input} autoComplete="off" />
+            {this.renderError(meta)}
         </div>
         );
     };
