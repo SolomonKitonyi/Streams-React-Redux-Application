c3db0225ba9ea6feb4bb6b29ea16e1fb1693debf
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-20 14:03:01 +0300

    modify connect function

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Field,reduxForm } from 'redux-form';
+import {connect} from 'react-redux';
+
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component{
     renderError({error,touched}){
@@ -46,7 +49,9 @@ const validate = (formValues)=>{
     return errors;
 };
 
-export default reduxForm({
+const formWrapped= reduxForm({
     form:'streamCreate',
     validate
 }) (StreamCreate);
+
+export default connect() (formWrapped);
