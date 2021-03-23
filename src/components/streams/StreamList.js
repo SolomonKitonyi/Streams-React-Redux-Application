cc67c6094b7ca9ed09d6d4c3e714d882b57ee9e6
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-23 12:53:21 +0300

    add styling

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -10,7 +10,12 @@ class StreamList extends React.Component{
 
     renderAdmin(stream){
         if(stream.userId === this.props.currentUserId){
-            return <div>Delete/Edit</div>
+            return (
+                <div className="ui right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            )
         }
     };
 
