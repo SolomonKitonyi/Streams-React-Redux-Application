5e7335312be602e136e4a3b8e6b2aba9ca59aa11
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-23 12:47:51 +0300

    add slight changes

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,6 +8,12 @@ class StreamList extends React.Component{
         this.props.fetchStreams()
     }
 
+    renderAdmin(stream){
+        if(stream.userId === this.props.currentUserId){
+            return <div>Delete/Edit</div>
+        }
+    };
+
     renderList (){
         return this.props.streams.map(stream=>{
             return (
@@ -17,6 +23,7 @@ class StreamList extends React.Component{
                         {stream.title}
                         <div className="description">{stream.description}</div>
                     </div>
+                    {this.renderAdmin(stream)}
                 </div>
             );
         });
@@ -31,7 +38,9 @@ class StreamList extends React.Component{
   }
 }
 const mapStateToProps = (state)=>{
-    return {streams: Object.values(state.streams)};
+    return {streams: Object.values(state.streams),
+        currentUserId:state.auth.userId
+    };
 }
 
 export default connect(mapStateToProps,{fetchStreams}) (StreamList);
