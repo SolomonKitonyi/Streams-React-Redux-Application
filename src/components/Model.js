303fcd266eadefeadf19f297f5144d0c83a2ba03
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-26 14:11:21 +0300

    add changes

---
+++ src/components/Model.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Model = props =>{
+    return ReactDom.createPortal(
+        <div className="ui dimmer modals visible active">
+            <div className="ui standard modal visible active">
+                Hello There I am a Modal
+            </div>
+        </div>,
+        document.querySelector('#modal')
+    )
+};
+
+export default Model;
