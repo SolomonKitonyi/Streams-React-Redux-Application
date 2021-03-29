eb3941fa99e68293ab5b2c5ad2b6818292742cba
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-29 12:00:55 +0300

    stop propagation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,7 +6,7 @@ import history from '../history';
 const Modal = props =>{
     return ReactDom.createPortal(
         <div onClick={()=>history.push('/')} className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
+            <div onClick={(e)=> e.stopPropagation()} className="ui standard modal visible active">
                 <div className="header">
                     Stream Delete
                 </div>
