42458e712b350b182b3025065253462c2f9dc443
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-29 14:16:07 +0300

    add delete

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -4,16 +4,18 @@ import {Link} from 'react-router-dom';
 
 import Modal from '../Modal';
 import history from '../../history';
-import {fetchStream} from '../../actions';
+import {fetchStream,deleteStream} from '../../actions';
 
 class StreamDelete extends React.Component{
     componentDidMount(){
        this.props.fetchStream(this.props.match.params.id);
     }
     renderActions(){
+        const id = this.props.match.params.id;
+
          return(
             <>
-                <button className="ui button negative">Delete</button>
+                <button onClick={()=>this.props.deleteStream(id)} className="ui button negative">Delete</button>
                 <Link to="/" className="ui button">Cancel</Link>
             </>
         );
@@ -42,4 +44,4 @@ const mapStateToProps= (state,ownProps)=>{
     return {stream:state.streams[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps,{fetchStream}) (StreamDelete);
+export default connect(mapStateToProps,{fetchStream,deleteStream}) (StreamDelete);
