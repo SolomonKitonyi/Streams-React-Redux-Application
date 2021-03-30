6bbb85aaa131b858d3d7ab7e8279c851410ea865
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-30 12:46:40 +0300

    add id to fetchStream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -5,7 +5,7 @@ import {fetchStream} from '../../actions';
 
 class StreamShow extends React.Component{
     componentDidMount(){
-        this.props.fetchStream()
+        this.props.fetchStream(this.props.match.params.id);
     }
     render(){
         return <div>StreamShow</div>
